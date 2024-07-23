affda4f163c9ba0a145b1acb5e95c397ce358ef9
L0rable <finwalker@gmail.com>
2024-07-23 13:18:29 +1000

    Update todo-item component and config

--- TodoListAppClient/src/app/todo-item/todo-item.component.ts
+++ TodoListAppClient/src/app/todo-item/todo-item.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { TodoItem } from '../todo-item';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-todo-item',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './todo-item.component.html',
   styleUrl: './todo-item.component.css'
 })
 export class TodoItemComponent {
-
+  @Input() item!: TodoItem;
+  @Output() remove = new EventEmitter<TodoItem>();
 }
