e828eb4b0178a7ddf1ba3846760d85768ee46307
L0rable <finwalker@gmail.com>
2024-07-23 13:12:29 +1000

    Update api service and todo-list component

--- TodoListAppClient/src/app/api.service.ts
+++ TodoListAppClient/src/app/api.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TodoItem } from './todo-item';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  private apiUrl = 'http://localhost:5206';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getTodoItem(): Observable<any> {
+    return this.http.get(this.apiUrl + '/todoItems');
+  }
+
+  addTodoItem(todoItemName: string): Observable<any> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.post(this.apiUrl + '/todoItems', {"name": todoItemName}, { headers });
+  }
+
+  deleteTodoItem(todoItem: TodoItem): Observable<any> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.delete(this.apiUrl + '/todoItems/' + todoItem.id, { headers });
+  }
 }
--- TodoListAppClient/src/app/todo-list/todo-list.component.ts
+++ TodoListAppClient/src/app/todo-list/todo-list.component.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
 import { TodoItem } from '../todo-item';
+import { ApiService } from '../api.service';
+import { TodoItemComponent } from '../todo-item/todo-item.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TodoItemComponent],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.css'
 })
 export class TodoListComponent {
+  todoItems: TodoItem[] = [];
+
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit(): void {
+    this.getTodoItems();
+  }
+
+  getTodoItems(): void {
+    this.apiService.getTodoItem().subscribe(data => {
+      this.todoItems = data;
+    },
+    error => error.log(error));
+  }
+  
+  addTodoItem(todoItemName: string) {
+    if (!todoItemName) return;
+
+    this.apiService.addTodoItem(todoItemName).subscribe({
+      next: (todoItem) => this.todoItems.push({
+        "id": todoItem.id,
+        "name": todoItem.name
+      }),
+      error: (e) => console.error(e)
+    });
+  }
+
+  deleteTodoItem(todoItem: TodoItem) {
+    this.apiService.deleteTodoItem(todoItem).subscribe({
+      next: () => this.todoItems.splice(this.todoItems.indexOf(todoItem), 1),
+      error: (e) => console.error(e)  
+    });
+  }
 }
