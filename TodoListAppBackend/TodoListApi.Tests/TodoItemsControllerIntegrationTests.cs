0a254f626a55723e4b37bc66769bb58ced850acc
L0rable <finwalker@gmail.com>
2024-07-23 14:46:57 +1000

    Add POST and DELETE integration tests

--- TodoListAppBackend/TodoListApi.Tests/TodoItemsControllerIntegrationTests.cs
+++ TodoListAppBackend/TodoListApi.Tests/TodoItemsControllerIntegrationTests.cs
@@ -1,4 +1,6 @@
+using System.Text;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
 using TodoItemApi;
 
 public class TodoItemsControllerIntegrationTests : IClassFixture<WebApplicationFactory<Program>>
@@ -22,5 +24,36 @@ public class TodoItemsControllerIntegrationTests : IClassFixture<WebApplicationF
         Assert.Contains("Task 1", responseString);
         Assert.Contains("Task 2", responseString);
         Assert.Contains("Task 3", responseString);
+    }
+
+    [Fact]
+    public async Task PostTodoItem_ReturnsCreatedStatusCode()
+    {
+        // Arrange
+        var newItem = new TodoItem { Name = "New Task" };
+        var json = JsonConvert.SerializeObject(newItem);
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+        // Act
+        var response = await _client.PostAsync("/todoItems", content);
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        var responseString = await response.Content.ReadAsStringAsync();
+        var createdItem = JsonConvert.DeserializeObject<TodoItem>(responseString);
+        Assert.Equal("New Task", createdItem.Name);
+    }
+
+    [Fact]
+    public async Task DeleteTodoItem_ReturnsSuccessStatusCode()
+    {
+        // Arrange
+        var itemId = 1;
+
+        // Act
+        var response = await _client.DeleteAsync("todoItems/" + itemId);
+
+        // Assert
+        response.EnsureSuccessStatusCode();
     }
 }
